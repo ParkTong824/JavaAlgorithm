3bf393fbf0056002a295e415c65c098e8acff69c
ParkTong824 <53634997+ParkTong824@users.noreply.github.com>
2019-10-18 14:34:16 +0900

    JadenCase 문자열 만들기
    JadenCase 문자열 만들기

--- src/javaAlgorithm2/Solution11.java
+++ src/javaAlgorithm2/Solution11.java
@@ -1,5 +1,47 @@
 package javaAlgorithm2;
 
 public class Solution11 {
+	public static String solution(String s) {
+	      String answer = "";
+	      char[] arrWord = new char[s.length()];
+	      int count = 0;
+	      //단어 구분위한 count 값
+	      boolean word = false;
+	      for(int i =0; i<s.length();i++) {
+	    	  arrWord[i] =(s.charAt(i));
+	      }
+	      while(count!=arrWord.length-1) {
+	    	  if((int)arrWord[count]==32) {
+	    		  count++;
+	    		  word = false;
+	    		  continue;
+	    	  }
+	    	  
+	    	  else if(word == false) {
+	    		  if((int)arrWord[count]>47 && (int)arrWord[count]<58) {
+	    			  count++;
+	    			  word = true;
+	    			  continue;
+	    		  }
+	    		  else if((int)arrWord[count]>96 && arrWord[count]<123) {
+	    			  arrWord[count]=(char)((int)arrWord[count]-32);
+	    			  count++;
+	    			  word = true;
+	    			  continue;
+	    		  }
+	    	  }
+	    	  
+	    	  else if((int)arrWord[count]<91 &&(int)arrWord[count]>64) {
+	    		  arrWord[count] = (char)((int)arrWord[count]+32);
+	    		  count++;
+	    	  }
+	    	  else {
+	    		  count++;
+	    	  }
+	      }
+	      String str = String.valueOf(arrWord);
+	      answer = str;
+	    return answer;
+	  }
 
 }

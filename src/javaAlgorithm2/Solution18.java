ba81db5b1ef5b6e63915b6ff05b5bb6b80090b38
ParkTong824 <53634997+ParkTong824@users.noreply.github.com>
2019-10-21 17:47:07 +0900

    카카오프렌즈 컬러링북

---
+++ src/javaAlgorithm2/Solution18.java
@@ -0,0 +1,18 @@
+package javaAlgorithm2;
+
+public class Solution18 {
+	public static void main(String[] args) {
+		
+	}
+	
+	public int[] solution(int m, int n, int[][] picture) {
+	      int numberOfArea = 0;
+	      int maxSizeOfOneArea = 0;
+	      
+	      int[] answer = new int[2];
+	      answer[0] = numberOfArea;
+	      answer[1] = maxSizeOfOneArea;
+	      return answer;
+	  }
+
+}

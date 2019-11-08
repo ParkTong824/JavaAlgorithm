f0a5ba024ad82c5a3a79ca70b0321e2d62cb8ba8
ParkTong824 <53634997+ParkTong824@users.noreply.github.com>
2019-11-08 17:27:14 +0900

    알고리즘 추가

---
+++ src/javaAlgorithm3/Solution01.java
@@ -0,0 +1,16 @@
+package javaAlgorithm3;
+
+import java.util.*;
+
+public class Solution01 {
+	public static void main(String[] args) {
+
+	}
+
+	public int[] solution(int[] heights) {
+		int[] answer = {};
+		Queue<Integer> heigh;
+	
+		return answer;
+	}
+}

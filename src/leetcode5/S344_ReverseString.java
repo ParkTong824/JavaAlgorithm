a98acee4ddb50b04569c1a7dda1589d3def8a766
SungWoo824 <scody1219@gmail.com>
2021-04-11 16:23:54 +0900

    fix : reverse string solution

--- src/leetcode5/S344_ReverseString.java
+++ src/leetcode5/S344_ReverseString.java
@@ -1,2 +1,22 @@
-package leetcode5;public class S344_ReverseString {
+package leetcode5;
+
+import java.util.Arrays;
+
+public class S344_ReverseString {
+    public static void reverseString(char[] s) {
+        int arrMaxIndex = s.length-1;
+        char[] tempArr = new char[s.length];
+        for (int i = 0 ; i < s.length ; i++) {
+            tempArr[i] = s[arrMaxIndex-i];
+            tempArr[arrMaxIndex-i] = s[i];
+        }
+        s = tempArr;
+        System.out.println(Arrays.toString(s));
+    }
+
+    public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o'};
+        reverseString(s);
+
+    }
 }

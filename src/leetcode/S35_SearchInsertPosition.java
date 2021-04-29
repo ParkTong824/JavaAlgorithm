14378675e21e66b85088c56d632dc62df46e7020
SungWoo824 <scody1219@gmail.com>
2021-04-29 23:12:47 +0900

    fix : 35 question to simple answer

--- src/leetcode/S35_SearchInsertPosition.java
+++ src/leetcode/S35_SearchInsertPosition.java
@@ -2,41 +2,23 @@ package leetcode;
 
 public class S35_SearchInsertPosition {
     public static void main(String[] args) {
-        int[] nums = {1,3,5,6};
+        int[] nums = {1, 3, 5, 6};
         int target = 7;
 
-        System.out.println(searchInsert(nums,target));
+        System.out.println(searchInsert(nums, target));
     }
+
     public static int searchInsert(int[] nums, int target) {
-        int startIndex = 0;
-        int finishIndex = nums.length-1;
-        while (true) {
-            int[] checkArr = isFront(nums,target,startIndex,finishIndex);
-            startIndex = checkArr[0];
-            finishIndex = checkArr[1];
-            if (finishIndex-startIndex==1){
-                return finishIndex;
-            }
-            if (checkArr[2]==0){
-                if (nums[checkArr[1]]==target){
-                    return checkArr[1];
-                }
+        int i = 0;
+        if (target <= nums[0]) {
+            return 0;
+        }
+        for (i = 0; i < nums.length - 1; i++) {
+            if (nums[i] < target && nums[i + 1] >= target) {
+                return ++i;
             }
         }
-
-
-    }       //  1 3 5 6 7 8     4-0 /2 = 2
-
-    public static int[] isFront(int[] nums, int target, int startIndex, int finishIndex){
-        int[] returnArr;
-        int searchIndex = (finishIndex - startIndex)/2;
-        boolean isFront = nums[startIndex+searchIndex] >=target;
-        if (isFront) {
-            returnArr = new int[]{startIndex, startIndex+searchIndex, 0};
-        } else {
-            returnArr = new int[]{startIndex+searchIndex,finishIndex, 1};
-        }
-        return returnArr;
+        return ++i;
     }
 }
 /*

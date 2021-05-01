8b46984756eea1d515da59782da22148c80321d5
SungWoo824 <scody1219@gmail.com>
2021-05-01 16:26:36 +0900

    fix : 43_Multiply didnt use BigInteger

--- src/leetcode/S34_2_MultiplyStrings.java
+++ src/leetcode/S34_2_MultiplyStrings.java
@@ -5,31 +5,61 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class S34_2_MultiplyStrings {
-    public String multiply(String num1, String num2) {
+    public static void main(String[] args) {
+        String num1 = "91";
+        String num2 = "999";
+
+        System.out.println(multiply(num1,num2));
+    }
+    public static String multiply(String num1, String num2) {
         StringBuffer answerBuffer = new StringBuffer();
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        if (num1.length()<num2.length()){
+            String temp = num2;
+            num2 = num1;
+            num1 = temp;
+        }
         int[] num1Arr = Arrays.stream(num1.split("")).mapToInt(Integer::parseInt).toArray();
         int[] num2Arr = Arrays.stream(num2.split("")).mapToInt(Integer::parseInt).toArray();
         int[] sumArr = new int[num1.length()];
-        Queue<int[]> arrQue = new LinkedList<>();
 
         int upPlus = 0;
-        for (int i = num2.length()-1 ; i >=0 ; i--) {
+        for (int i = num2.length() - 1; i >= 0; i--) {
             int index = 0;
-            if (!arrQue.isEmpty()){
-                sumArr=arrQue.poll();
-            }
-            for (int j = num1.length()-1 ; j >=0 ; j--){
-                if (j == num1.length()-1){
-                    int multiple = num1Arr[j]*num2Arr[i]+upPlus;
-                    upPlus = multiple/10;
-                    answerBuffer.insert(0,upPlus%10);
-                    continue;
+            for (int j = num1.length() - 1; j >= 0; j--) {
+                int multiple = 0;
+                if (index == 0) {
+                    multiple = sumArr[index] + num1Arr[j] * num2Arr[i];
+                    answerBuffer.insert(0, multiple % 10);
+                    upPlus = multiple / 10;
+                    index++;
+                } else {
+                    multiple = (num1Arr[j] * num2Arr[i]) + sumArr[index] + upPlus;
+                    sumArr[index - 1] = multiple % 10;
+                    upPlus = multiple / 10;
+                    index++;
                 }
-//                sumArr[index] =
+                if (j == 0) {
+                    sumArr[index-1] = upPlus;
+                    upPlus=0;
+                    index = 0;
+                }
             }
         }
+        for (int i = 0; i < sumArr.length; i++) {
+            if (i==sumArr.length-1){
+                sumArr[i] += upPlus;
+                if (sumArr[i]!=0) {
+                    answerBuffer.insert(0, sumArr[i]);
+                }
+                continue;
+            }
+            answerBuffer.insert(0, sumArr[i]);
+        }
 
-
+        return answerBuffer.toString();
     }
 }
 /*

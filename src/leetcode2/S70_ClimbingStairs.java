0085a637188347f969607c399a40c23db082664f
SungWoo824 <scody1219@gmail.com>
2021-05-15 00:03:02 +0900

    add : 70_ClimbingStairs

---
+++ src/leetcode2/S70_ClimbingStairs.java
@@ -0,0 +1,26 @@
+package leetcode2;
+
+public class S70_ClimbingStairs {
+    public static void main(String[] args) {
+
+    }
+    public int climbStairs(int n) {
+        
+    }
+}
+/*
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+1 <= n <= 45
+ */

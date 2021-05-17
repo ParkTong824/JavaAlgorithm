8da7a6a6555c3bae0213267599450e14e6826d38
SungWoo824 <scody1219@gmail.com>
2021-05-17 20:01:59 +0900

    fix : 70_ClimbingStairs

--- src/leetcode2/S70_ClimbingStairs.java
+++ src/leetcode2/S70_ClimbingStairs.java
@@ -2,10 +2,20 @@ package leetcode2;
 
 public class S70_ClimbingStairs {
     public static void main(String[] args) {
-
+        int n = 4;
+        System.out.println(climbStairs(n));
     }
-    public int climbStairs(int n) {
-
+    public static int climbStairs(int n) {
+        return climbStair(0,n);
+    }
+    public static int climbStair(int i, int n){
+        if (i > n){
+            return 0;
+        }
+        if (i == n){
+            return 1;
+        }
+        return climbStair(i+1,n)+climbStair(i+2,n);
     }
 }
 /*
@@ -21,6 +31,23 @@ Explanation: There are three ways to climb to the top.
 1. 1 step + 1 step + 1 step
 2. 1 step + 2 steps
 3. 2 steps + 1 step
+
+4  =>  5
+1 1 1 1
+2 1 1
+1 2 1
+1 1 2
+2 2
+
+5  => 8
+1 1 1 1 1
+1 1 1 2
+1 1 2 1
+1 2 1 1
+2 1 1 1
+2 2 1
+2 1 2
+1 2 2
 
 1 <= n <= 45
  */

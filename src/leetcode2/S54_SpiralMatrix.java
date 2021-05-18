e6f48b7c0aab7dc9fda3af62777872831d85e5e8
SungWoo824 <scody1219@gmail.com>
2021-05-18 22:36:23 +0900

    fix : 54_SpiralMatrix

--- src/leetcode2/S54_SpiralMatrix.java
+++ src/leetcode2/S54_SpiralMatrix.java
@@ -3,11 +3,15 @@ package leetcode2;
 import java.util.List;
 
 public class S54_SpiralMatrix {
-//    public List<Integer> spiralOrder(int[][] matrix) {
-//
-//    }
+    public List<Integer> spiralOrder(int[][] matrix) {
+        while (true) {
+            
+        }
+    }
 
     public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
 
+        System.out.println(matrix[0][1]);
     }
 }

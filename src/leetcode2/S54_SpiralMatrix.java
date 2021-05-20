2411e50d5fb9acd558268fb83dffe18370b4ad13
SungWoo824 <scody1219@gmail.com>
2021-05-20 21:46:13 +0900

    fix : SpiralMatrix

--- src/leetcode2/S54_SpiralMatrix.java
+++ src/leetcode2/S54_SpiralMatrix.java
@@ -1,17 +1,50 @@
 package leetcode2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class S54_SpiralMatrix {
-    public List<Integer> spiralOrder(int[][] matrix) {
-        while (true) {
-            
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> answer = new ArrayList<>();
+        int horLength = matrix[0].length;
+        int verLength = matrix.length-1;
+        boolean isUp = false;
+        boolean isLeft = false;
+        int nowHor = -1;
+        int nowVer = 0;
+
+        while (horLength>=0 && verLength>=0) {
+            if (horLength>0){
+                for (int i = 0 ; i < horLength ; i++) {
+                    if (isLeft){
+                        nowHor--;
+                    } else {
+                        nowHor++;
+                    }
+                    answer.add(matrix[nowVer][nowHor]);
+                }
+            }
+            isLeft=!isLeft;
+            horLength--;
+            if (verLength>0){
+                for (int i = 0 ; i < verLength ; i++) {
+                    if (isUp){
+                        nowVer--;
+                    } else {
+                        nowVer++;
+                    }
+                    answer.add(matrix[nowVer][nowHor]);
+                }
+            }
+            isUp=!isUp;
+            verLength--;
         }
+        return answer;
     }
 
     public static void main(String[] args) {
-        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        int[][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
 
-        System.out.println(matrix[0][1]);
+        System.out.println(spiralOrder(matrix).toString());
     }
 }

16331ed083ed97e6c4d5cf7aa929882649f001a8
SungWoo824 <scody1219@gmail.com>
2021-05-26 21:22:51 +0900

    add : SpiralMatrix2

---
+++ src/leetcode2/S59_SpiralMatrix2.java
@@ -0,0 +1,18 @@
+package leetcode2;
+
+public class S59_SpiralMatrix2 {
+    public static void main(String[] args) {
+
+    }
+    public int[][] generateMatrix(int n) {
+
+    }
+
+}
+/*
+Input: n = 3
+Output: [[1,2,3],[8,9,4],[7,6,5]]
+
+Input: n = 1
+Output: [[1]]
+ */

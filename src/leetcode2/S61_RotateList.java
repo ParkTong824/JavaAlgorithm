a266be98ad943fc310bdae6502eec8cb83d9c7d8
SungWoo824 <scody1219@gmail.com>
2021-05-28 21:48:39 +0900

    add : RotateList

---
+++ src/leetcode2/S61_RotateList.java
@@ -0,0 +1,24 @@
+package leetcode2;
+
+public class S61_RotateList {
+    public ListNode rotateRight(ListNode head, int k) {
+
+    }
+}
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/*
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+
+Input: head = [0,1,2], k = 4
+Output: [2,0,1]
+ */

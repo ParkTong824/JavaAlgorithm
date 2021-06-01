af57b3175500601bfe94cf3a4c0dd96df3ad45e6
SungWoo824 <scody1219@gmail.com>
2021-06-01 21:15:57 +0900

    add : SetMatrixZeores problem add

---
+++ src/leetcode2/S73_SetMatrixZeroes.java
@@ -0,0 +1,14 @@
+package leetcode2;
+
+public class S73_SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+
+    }
+}
+/*
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ */

1d593714962779242086a4d6e3dd01a778ee4cbe
SungWoo824 <scody1219@gmail.com>
2021-06-02 22:10:38 +0900

    add : SearchMatrix problem add

---
+++ src/leetcode2/S74_SearchMatrix.java
@@ -0,0 +1,10 @@
+package leetcode2;
+
+public class S74_SearchMatrix {
+    public static void main(String[] args) {
+
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+    }
+}

609f53242dbab3c6a4eba1dcaaa09525d7c13f1d
SungWoo824 <scody1219@gmail.com>
2021-06-05 12:40:37 +0900

    add : solved setMatrixZeroes

--- src/leetcode2/S73_SetMatrixZeroes.java
+++ src/leetcode2/S73_SetMatrixZeroes.java
@@ -1,13 +1,40 @@
 package leetcode2;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class S73_SetMatrixZeroes {
-    public void setZeroes(int[][] matrix) {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 1, 1}, {1, 0, 1},{1,1,1}};
+        setZeroes(matrix);
+    }
+    public static int[][] setZeroes(int[][] matrix) {
         int[][] answer =new int[matrix.length][matrix[0].length];
+        Set<Integer> horSet = new HashSet<>();
+        Set<Integer> verSet = new HashSet<>();
+
         for (int i = 0 ; i < matrix.length ; i++){
             for (int j = 0 ; j < matrix[0].length; j++){
-
+                if(matrix[i][j]==0){
+                    horSet.add(i);
+                    verSet.add(j);
+                    answer[i][j]=0;
+                }
             }
         }
+        for (int i = 0 ; i < matrix.length ; i++){
+            if (horSet.contains(i)){
+                continue;
+            }
+            for (int j = 0 ; j < matrix[0].length; j++){
+                if (verSet.contains(j)){
+                    continue;
+                }
+                answer[i][j]=matrix[i][j];
+            }
+        }
+        return answer;
     }
 }
 /*

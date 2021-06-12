bc5d6629b6558e9e2122f77cfb12643e21a7627a
SungWoo824 <scody1219@gmail.com>
2021-06-12 20:51:42 +0900

    fix : combination

--- src/leetcode2/S77_Combination.java
+++ src/leetcode2/S77_Combination.java
@@ -1,2 +1,33 @@
-package leetcode2;public class S77_Combination {
+package leetcode2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class S77_Combination {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> answer = new ArrayList<>();
+
+        for (int i = 0 ; i < n ; i ++){
+            
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
+/*
+Input: n = 4, k = 2
+Output:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+
+Input: n = 1, k = 1
+Output: [[1]]
+ */

f389454925f7476fbd144fd2c5f8b692951d915f
SungWoo824 <scody1219@gmail.com>
2021-06-13 17:42:05 +0900

    add : solve Combination

--- src/leetcode2/S77_Combination.java
+++ src/leetcode2/S77_Combination.java
@@ -5,10 +5,26 @@ import java.util.List;
 
 public class S77_Combination {
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> answer = new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
 
-        for (int i = 0 ; i < n ; i ++){
-            
+        getCombination(1,n,k,new ArrayList<>(),ans);
+        return ans;
+
+    }
+    public static void getCombination(int s,int d,int size,ArrayList<Integer> a,List<List<Integer>> ans)
+    {
+        if(a.size()>=size)
+        {
+            if(a.size()==size){
+                ans.add(new ArrayList<>(a));
+            }
+            return ;
+        }
+        for(int i=s;i<=d;i++)
+        {
+            a.add(i);
+            getCombination(i+1,d,size,a,ans);
+            a.remove(a.size()-1);
         }
     }
 

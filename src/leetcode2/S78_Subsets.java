9311a45d22f2d6265bec88a77cba22c8d0042993
SungWoo824 <scody1219@gmail.com>
2021-06-14 22:21:16 +0900

    add : subset problem

---
+++ src/leetcode2/S78_Subsets.java
@@ -0,0 +1,17 @@
+package leetcode2;
+
+import java.util.List;
+
+public class S78_Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+
+    }
+}
+/*
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+ */

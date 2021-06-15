b5f5b33cfa910760b1b37c694f3629e1c29af320
SungWoo824 <scody1219@gmail.com>
2021-06-15 20:11:15 +0900

    add : subset solution

--- src/leetcode2/S78_Subsets.java
+++ src/leetcode2/S78_Subsets.java
@@ -1,16 +1,28 @@
 package leetcode2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class S78_Subsets {
     public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> output = new ArrayList();
+        output.add(new ArrayList<Integer>());
 
+        for (int num : nums) {
+            List<List<Integer>> newSubsets = new ArrayList();
+            for (List<Integer> curr : output) {
+                newSubsets.add(new ArrayList<Integer>(curr){{add(num);}});
+            }
+            for (List<Integer> curr : newSubsets) {
+                output.add(curr);
+            }
+        }
+        return output;
     }
 }
 /*
 Input: nums = [1,2,3]
 Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
-Example 2:
 
 Input: nums = [0]
 Output: [[],[0]]

476522cf0afd000f203d8cf03def385c1b8390bc
SungWoo824 <scody1219@gmail.com>
2021-06-20 21:53:07 +0900

    fix : MergeSortArr fix solution

--- src/leetcode2/S88_MergeSortArr.java
+++ src/leetcode2/S88_MergeSortArr.java
@@ -3,7 +3,17 @@ package leetcode2;
 import java.util.Arrays;
 
 public class S88_MergeSortArr {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m =3;
+        int[] nums2 = {2, 5, 6};
+        int n =3;
+
+        merge(nums1,m,nums2,n);
+
+        System.out.println(Arrays.toString(nums1));
+    }
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
         if (nums1.length!=m) {
             if (nums1.length==n){
                 nums1 = Arrays.copyOfRange(nums2,0,n);
@@ -11,20 +21,37 @@ public class S88_MergeSortArr {
                 int[] tempArr = nums1.clone();
                 int firstIndex = 0;
                 int secondIndex = 0;
-                while (m>0&&n>0){
-
-                    int fisrtNum=0;
-                    int secondNum=0;
+                int firstNum;
+                int secondNum;
+                while (m>0||n>0){
                     if (m>0){
-
+                        firstNum = tempArr[firstIndex];
+                        if (n>0){
+                            secondNum=nums2[secondIndex];
+                            if (firstNum>secondNum){
+                                nums1[firstIndex+secondIndex]=secondNum;
+                                secondIndex++;
+                                n--;
+                            } else {
+                                nums1[firstIndex+secondIndex]=firstNum;
+                                firstIndex++;
+                                m--;
+                            }
+                        } else {
+                            nums1[firstIndex+secondIndex]=firstNum;
+                            firstIndex++;
+                        }
+                    } else {
+                        secondNum=nums2[secondIndex];
+                        nums1[firstIndex+secondIndex]=secondNum;
+                        secondIndex++;
+                        n--;
                     }
                 }
             }
         }
 
     }
-
-
 }
 /*
 Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3

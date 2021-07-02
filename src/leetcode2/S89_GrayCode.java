9b1bd0ac9b6b50fe3d90c39841176596e72c598f
SungWoo824 <scody1219@gmail.com>
2021-07-02 20:34:37 +0900

    add : gray code Solution

---
+++ src/leetcode2/S89_GrayCode.java
@@ -0,0 +1,34 @@
+package leetcode2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class S89_GrayCode {
+    public List<Integer> grayCode(int n) {
+        String[] grayCodes = binaryGrayCodes(n);
+        List<Integer> result = new ArrayList<>();
+        for(String grayCode: grayCodes){
+            result.add(Integer.parseInt(grayCode,2));
+        }
+        return result;
+    }
+
+    private String[] binaryGrayCodes(int n) {
+        int total = (int) Math.pow(2,n);
+        String[] grayCode = new String[total];
+        grayCode[0]="0";
+        grayCode[1]="1";
+
+        for(int i=2;i<=n;i++){
+            int start = (int) Math.pow(2,i-1);
+            for(int j=0;j<start;j++){
+                if(grayCode[start-j-1].length()<i-1){
+                    grayCode[start-j-1] = "0".concat(grayCode[start-j-1]);
+                }
+                grayCode[start+j]="1".concat(grayCode[start-j-1]);
+            }
+        }
+
+        return grayCode;
+    }
+}

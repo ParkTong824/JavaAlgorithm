79f5ce581927f6f4821ef04354132aac7b6b05a7
SungWoo824 <scody1219@gmail.com>
2021-07-05 20:30:06 +0900

    add: problem pascalTriangle

---
+++ src/leetcode3/S118_PascalTriangle.java
@@ -0,0 +1,15 @@
+package leetcode3;
+
+public class S118_PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+
+    }
+}
+/*
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Example 2:
+
+Input: numRows = 1
+Output: [[1]]
+ */

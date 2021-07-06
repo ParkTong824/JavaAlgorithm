2dad935abfd2aa04b70bf97a877415813bdb33fc
SungWoo824 <scody1219@gmail.com>
2021-07-06 22:13:45 +0900

    add : pascal Solution

--- src/leetcode3/S118_PascalTriangle.java
+++ src/leetcode3/S118_PascalTriangle.java
@@ -1,13 +1,51 @@
 package leetcode3;
 
-public class S118_PascalTriangle {
-    public List<List<Integer>> generate(int numRows) {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class S118_PascalTriangle {
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> answer = new ArrayList<>();
+        for (int i = 0 ; i < numRows ; i++) {
+            if (i == 0){
+                answer.add(new ArrayList<>(Arrays.asList(1)));
+                continue;
+            } else if (i ==1){
+                answer.add(new ArrayList<>(Arrays.asList(1,1)));
+                continue;
+            }
+            List<Integer> result = new ArrayList<>();
+            for (int j = 0 ; j <i+1 ; j++){
+                if (j==0 || j==i){
+                    result.add(1);
+                } else {
+                    result.add(answer.get(i-1).get(j-1)+answer.get(i-1).get(j));
+                }
+            }
+            answer.add(result);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> answer = generate(5);
+        System.out.println(answer.get(0).toString());
+        System.out.println(answer.get(1).toString());
+        System.out.println(answer.get(2).toString());
+        System.out.println(answer.get(3).toString());
+        System.out.println(answer.get(4).toString());
     }
 }
 /*
 Input: numRows = 5
-Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Output: [
+    [1],
+   [1,1],
+  [1,2,1],
+ [1,3,3,1],
+[1,4,6,4,1]]
 Example 2:
 
 Input: numRows = 1

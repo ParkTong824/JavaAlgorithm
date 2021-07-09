e85ad94e301711d627da76c2aa6b2f011c655b3b
SungWoo824 <scody1219@gmail.com>
2021-07-09 23:11:52 +0900

    add : problem and solution

---
+++ src/leetcode3/S122_BestTimeBuySell2.java
@@ -0,0 +1,35 @@
+package leetcode3;
+
+public class S122_BestTimeBuySell2 {
+    public static int maxProfit(int[] prices) {
+        int answer = 0;
+        for (int i = 1 ; i < prices.length ; i++){
+            if (prices[i]>prices[i-1]){
+                answer += prices[i]-prices[i-1];
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+}
+/*
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Example 2:
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging multiple transactions at the same time. You must sell before buying again.
+Example 3:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e., max profit = 0.
+ */

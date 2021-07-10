0c6eb0f0d013197e1c20d586719b6e688b464e4e
SungWoo824 <scody1219@gmail.com>
2021-07-10 19:23:47 +0900

    add : problem and fix sample

---
+++ src/leetcode3/S125_ValidPalindrome.java
@@ -0,0 +1,17 @@
+package leetcode3;
+
+public class S125_ValidPalindrome {
+    public boolean isPalindrome(String s) {
+
+    }
+}
+/*
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+ */

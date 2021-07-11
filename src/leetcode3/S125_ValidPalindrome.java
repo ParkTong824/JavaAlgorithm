67840e8905291935c642b77aede8e9664bdf76f0
SungWoo824 <scody1219@gmail.com>
2021-07-11 17:14:02 +0900

    add : is validation solution

--- src/leetcode3/S125_ValidPalindrome.java
+++ src/leetcode3/S125_ValidPalindrome.java
@@ -1,8 +1,36 @@
 package leetcode3;
 
 public class S125_ValidPalindrome {
-    public boolean isPalindrome(String s) {
+    public static boolean isPalindrome(String s) {
+        int front = 0;
+        int tail = s.length()-1;
+        s = s.toLowerCase();
+        while (front<tail){
+            boolean isAlpha = false;
+            if (s.charAt(front)<'a' || s.charAt(front)>'z'){
+                front++;
+                isAlpha=true;
+            }
+            if (s.charAt(tail)<'a' || s.charAt(tail)>'z'){
+                tail--;
+                isAlpha=true;
+            }
+            if(isAlpha){
+                continue;
+            }
+            if(s.charAt(front)!=s.charAt(tail)){
+                return false;
+            } else {
+                front++;
+                tail--;
+            }
+        }
+        return true;
+    }
 
+    public static void main(String[] args) {
+        String s = "race a car";
+        System.out.println(isPalindrome(s));
     }
 }
 /*

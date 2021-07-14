62f9c21d316016dd35e644291f98e02fd0900c7c
SungWoo824 <scody1219@gmail.com>
2021-07-14 17:55:16 +0900

    add : problem

---
+++ src/leetcode2/S91_DecodeWays.java
@@ -0,0 +1,29 @@
+package leetcode2;
+
+public class S91_DecodeWays {
+    public static void main(String[] args) {
+
+    }
+    public int numDecodings(String s) {
+
+    }
+}
+/*
+Input: s = "12"
+Output: 2
+Explanation: "12" could be decoded as "AB" (1 2) or "L" (12).
+
+Input: s = "226"
+Output: 3
+Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+
+Input: s = "0"
+Output: 0
+Explanation: There is no character that is mapped to a number starting with 0.
+The only valid mappings with 0 are 'J' -> "10" and 'T' -> "20", neither of which start with 0.
+Hence, there are no valid ways to decode this since all digits need to be mapped.
+
+Input: s = "06"
+Output: 0
+Explanation: "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06").
+ */

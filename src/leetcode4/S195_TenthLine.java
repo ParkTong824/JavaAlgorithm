fb278dd1df52d4357118c433c1bdd59f73f090de
SungWoo824 <scody1219@gmail.com>
2021-07-20 18:53:32 +0900

    add : file reader solution

---
+++ src/leetcode4/S195_TenthLine.java
@@ -0,0 +1,22 @@
+package leetcode4;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class S195_TenthLine {
+    public static void main(String[] args) {
+        File file = new File("/Users/user/test.txt");
+        int lineNum = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lineNum++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Line "+lineNum);
+    }
+}

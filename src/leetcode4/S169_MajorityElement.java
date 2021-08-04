a967e1ed3b13dfe6abcfbfd68b5088869f84bc7e
SungWoo824 <scody1219@gmail.com>
2021-08-04 17:26:13 +0900

    fix : solution

--- src/leetcode4/S169_MajorityElement.java
+++ src/leetcode4/S169_MajorityElement.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class S169_MajorityElement {
-    public int majorityElement(int[] nums) {
+    public static int majorityElement(int[] nums) {
         int answer = 0;
         Map<Integer,Integer> numMap = new HashMap<>();
         for (int i = 0 ; i < nums.length ; i++) {
@@ -17,17 +17,18 @@ public class S169_MajorityElement {
         }
         for (int key : numMap.keySet()){
             if(answer==0){
-                answer = numMap.get(key);
+                answer = key;
                 continue;
             }
-            answer = answer>numMap.get(key)?answer:numMap.get(key);
+            answer = numMap.get(answer)>numMap.get(key)?answer:key;
         }
 
         return answer;
     }
 
     public static void main(String[] args) {
-
+        int[] nums = {3,2,3};
+        System.out.println(majorityElement(nums));
     }
 }
 /*

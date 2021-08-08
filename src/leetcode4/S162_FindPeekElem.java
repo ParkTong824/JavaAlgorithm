ee496323df0d1020769bd01087a57a1465d70345
SungWoo824 <scody1219@gmail.com>
2021-08-08 17:27:22 +0900

    add : problem

---
+++ src/leetcode4/S162_FindPeekElem.java
@@ -0,0 +1,21 @@
+package leetcode4;
+
+public class S162_FindPeekElem {
+    public int findPeakElement(int[] nums) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+
+Input: nums = [1,2,1,3,5,6,4]
+Output: 5
+ */

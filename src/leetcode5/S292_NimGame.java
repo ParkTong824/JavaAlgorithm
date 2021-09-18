49d3a14302dae39e7cd33bc0ba67ed86673dfc78
SungWoo824 <scody1219@gmail.com>
2021-09-18 15:43:34 +0900

    add : problem and solution

---
+++ src/leetcode5/S292_NimGame.java
@@ -0,0 +1,18 @@
+package leetcode5;
+
+public class S292_NimGame {
+    public boolean canWinNim(int n) {
+        return n%4!=0;
+    }
+}
+/*
+Input: n = 4
+Output: false
+
+Input: n = 1
+Output: true
+Example 3:
+
+Input: n = 2
+Output: true
+ */

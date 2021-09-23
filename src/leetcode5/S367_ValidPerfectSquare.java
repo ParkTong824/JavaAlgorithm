58c6ce123137cd02800302ec983d5250abedd578
SungWoo824 <scody1219@gmail.com>
2021-09-23 13:54:48 +0900

    fix : solution fix for long int

--- src/leetcode5/S367_ValidPerfectSquare.java
+++ src/leetcode5/S367_ValidPerfectSquare.java
@@ -2,7 +2,19 @@ package leetcode5;
 
 public class S367_ValidPerfectSquare {
     public boolean isPerfectSquare(int num) {
+        int numLength = (int)(Math.log10(num)+1);
         int startNum = 1;
+        int answerLength = 0;
+        if (numLength>2){
+            if (numLength%2==1){
+                answerLength=numLength/2+1;
+            } else {
+                answerLength=numLength/2;
+            }
+            for (int i = 0 ; i < answerLength ; i++){
+                startNum*=10;
+            }
+        }
         while (true){
             int compareNum = startNum*startNum;
             if (num==compareNum){
@@ -10,7 +22,7 @@ public class S367_ValidPerfectSquare {
             } else if (num<compareNum){
                 return false;
             } else {
-                startNum++;
+                    startNum++;
             }
         }
     }

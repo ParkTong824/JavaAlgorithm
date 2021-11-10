084502b6efba5095b3c25406801c9c048e044b98
SungWoo824 <scody1219@gmail.com>
2021-11-10 19:32:25 +0900

    add : problem

---
+++ src/leetcode5/S401_BinaryWatch.java
@@ -0,0 +1,14 @@
+package leetcode5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class S401_BinaryWatch {
+    public List<String> readBinaryWatch(int turnedOn) {
+        if (turnedOn==0 || turnedOn>=9) {
+            return new ArrayList<>();
+        }
+        int hourMax = 3;
+        int minMax = 5;
+    }
+}

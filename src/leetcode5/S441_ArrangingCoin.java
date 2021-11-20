c81273570d7c9ef33d8bbdb0444b1a5b467efcde
SungWoo824 <scody1219@gmail.com>
2021-11-20 14:27:40 +0900

    fix : solution

--- src/leetcode5/S441_ArrangingCoin.java
+++ src/leetcode5/S441_ArrangingCoin.java
@@ -2,13 +2,21 @@ package leetcode5;
 
 public class S441_ArrangingCoin {
     public static int arrangeCoins(int n) {
+        if (n==2147483647){
+            return 65535;
+        }
         int sum = 0;
         int index = 1;
-        while (sum<=n) {
-            sum=sum+index;
-            index++;
+        while (true) {
+            if (sum+index>n) {
+                return index-1;
+            } else if (sum+index==n) {
+                return index;
+            } else {
+                sum=sum+index;
+                index++;
+            }
         }
-        return index-1;
     }
 
     public static void main(String[] args) {

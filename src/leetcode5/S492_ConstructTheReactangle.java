826467302d382083f4cd534eb6bf16cc4bd4bb71
SungWoo824 <scody1219@gmail.com>
2021-12-10 23:51:11 +0900

    add : problem

---
+++ src/leetcode5/S492_ConstructTheReactangle.java
@@ -0,0 +1,19 @@
+package leetcode5;
+
+public class S492_ConstructTheReactangle {
+    public int[] constructRectangle(int area) {
+
+    }
+}
+/*
+Input: area = 4
+Output: [2,2]
+Explanation: The target area is 4, and all the possible ways to construct it are [1,4], [2,2], [4,1].
+But according to requirement 2, [1,4] is illegal; according to requirement 3,  [4,1] is not optimal compared to [2,2]. So the length L is 2, and the width W is 2.
+
+Input: area = 37
+Output: [37,1]
+
+Input: area = 122122
+Output: [427,286]
+ */

373c6962f922c3421c315dcbbdcc9e9fdf7a5f31
Sungwoo824 <scody1219@gmail.com>
2022-05-05 20:11:36 +0900

    add : leetcode problem

--- src/leetcode5/S268_MissingNumber.java
+++ src/leetcode5/S268_MissingNumber.java
@@ -1,2 +1,31 @@
-package leetcode5;public class S268_MissingNumber {
+package leetcode5;
+
+public class S268_MissingNumber {
+    public static int missingNumber(int[] nums) {
+        int sum = 0;
+        for (int i : nums) {
+            sum +=i;
+        }
+        int answer = nums.length*(nums.length+1)/2 - sum;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {9,6,4,2,3,5,7,0,1};
+        System.out.println(missingNumber(nums));
+    }
 }
+/*
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+ */

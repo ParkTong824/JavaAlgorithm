68363fd8c4b52bb676fa86fc8ce1165a17603076
Sungwoo824 <scody1219@gmail.com>
2022-05-09 11:21:53 +0900

    add : solution

--- src/test/Solution13.java
+++ src/test/Solution13.java
@@ -1,2 +1,25 @@
-package test;public class Solution13 {
+package test;
+
+public class Solution13 {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
+/*
+0~9 까지 정수를 가진 배열 numbers가 주어집니다. 배열 numbers가 매개변수로 주어질 때,
+numbers에 있는 자연수에 부호(+,-)를 붙여 전체 합이 0으로 되는 가지 수를 return 하도록 solution 함수를 완성해주세요.
+
+제한사항
+배열 numbers의 원소 개수는 100개 이하의 자연수입니다.
+배열 안에 있는 정수는 0 이상 9 이하입니다.
+정답이 커질 수 있으므로 마지막 5자리만 return 해주세요.
+입출력 예
+numbers	result
+[1, 1, 1, 1]	6
+[8, 3, 5, 7, 3, 4]	6
+ */

a926c2c4f9f76cd9701bd6e991401abf048bd46b
Sungwoo824 <scody1219@gmail.com>
2022-05-11 21:52:13 +0900

    fix : solution

--- src/leetcode5/S405_ConvertANumberToHexa.java
+++ src/leetcode5/S405_ConvertANumberToHexa.java
@@ -2,52 +2,15 @@ package leetcode5;
 
 public class S405_ConvertANumberToHexa {
     public static void main(String[] args) {
-        int num = 32;
-        System.out.println(Integer.toHexString(num));
+        int num = 16*16;
+        System.out.println(toHex(num));
     }
     public static String toHex(int num) {
-        int resultLength = 1;
-        StringBuilder result = new StringBuilder();
-        while (true) {
-            if (num<Math.pow(16,resultLength)){
-                break;
-            } else {
-                resultLength++;
-            }
-        }
-        for (int i = 0 ; i < resultLength ; i++) {
-            int indexNum = (int) (num/Math.pow(16,resultLength-i-1));
-            result.append(convertIntToString(indexNum));
-            num%=(int)(num/Math.pow(16,resultLength-i-1));
+        int leng = 0;
+        while (Math.pow(16,leng)<=num) {
+            leng++;
         }
 
-        return result.toString();
-    }
-    public static StringBuilder convertIntToString(int num){
-        StringBuilder convertString = new StringBuilder();
-        switch (num){
-            case 10:
-                convertString.append("a");
-                break;
-            case 11:
-                convertString.append("b");
-                break;
-            case 12:
-                convertString.append("c");
-                break;
-            case 13:
-                convertString.append("d");
-                break;
-            case 14:
-                convertString.append("e");
-                break;
-            case 15:
-                convertString.append("f");
-                break;
-            default:
-                convertString.append(num);
-                break;
-        }
-        return convertString;
+        return String.valueOf(leng);
     }
 }

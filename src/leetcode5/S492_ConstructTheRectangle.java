7d789f36a96f67b5d5a9569309f66ab64f567df6
Sungwoo824 <scody1219@gmail.com>
2022-05-13 21:52:13 +0900

    add : problem

---
+++ src/leetcode5/S492_ConstructTheRectangle.java
@@ -0,0 +1,11 @@
+package leetcode5;
+
+public class S492_ConstructTheRectangle {
+    public static void main(String[] args) {
+
+    }
+    public int[] constructRectangle(int area) {
+
+        return null;
+    }
+}

fbb03c26f728ad562ef98b3bcfab57c08f376b68
Sungwoo824 <scody1219@gmail.com>
2022-05-14 21:52:13 +0900

    add : solution

--- src/leetcode5/S492_ConstructTheRectangle.java
+++ src/leetcode5/S492_ConstructTheRectangle.java
@@ -1,11 +1,18 @@
 package leetcode5;
 
+import java.util.Arrays;
+
 public class S492_ConstructTheRectangle {
     public static void main(String[] args) {
-
+        int a = 122122;
+        System.out.println(Arrays.toString(constructRectangle(a)));
     }
-    public int[] constructRectangle(int area) {
+    public static int[] constructRectangle(int area) {
+        int middleNum = (int)Math.sqrt(area);
+        while (area%middleNum!=0) {
+            middleNum--;
+        }
 
-        return null;
+        return new int[]{area / middleNum, middleNum};
     }
 }

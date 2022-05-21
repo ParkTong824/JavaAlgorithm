4f92b1749789d875369e4c11ed71f838d16a8dab
Sungwoo824 <scody1219@gmail.com>
2022-05-21 21:52:13 +0900

    add : solution

--- src/leetcode6/S507_PerfectNumber.java
+++ src/leetcode6/S507_PerfectNumber.java
@@ -1,11 +1,34 @@
 package leetcode6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class S507_PerfectNumber {
     public boolean checkPerfectNumber(int num) {
+        int checkNum = 2;
+        List<Integer> divisorArr = new ArrayList<>();
+        divisorArr.add(1);
+        while (checkNum <= Math.sqrt(num)) {
+            if (num%checkNum == 0) {
+                divisorArr.add(checkNum);
+                divisorArr.add(num/checkNum);
+            }
+            checkNum++;
+        }
 
+        return false;
     }
 
     public static void main(String[] args) {
 
     }
 }
+/*
+Input: num = 28
+Output: true
+Explanation: 28 = 1 + 2 + 4 + 7 + 14
+1, 2, 4, 7, and 14 are all divisors of 28.
+
+Input: num = 7
+Output: false
+ */

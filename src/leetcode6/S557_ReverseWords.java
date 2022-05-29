8828e9e2ce179f9ec068f043c9436f682a350bb3
Sungwoo824 <scody1219@gmail.com>
2022-05-29 21:52:13 +0900

    add : solution

--- src/leetcode6/S557_ReverseWords.java
+++ src/leetcode6/S557_ReverseWords.java
@@ -1,9 +1,23 @@
 package leetcode6;
 
 public class S557_ReverseWords {
-    public String reverseWords(String s) {
-
-        return null;
+    public static void main(String[] args) {
+        String test = "Let's take LeetCode contest";
+        System.out.println(reverseWords(test));
+    }
+    public static String reverseWords(String s) {
+        String[] splitArr = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (int i = 0 ; i < splitArr.length ; i++) {
+            char[] tempArr = splitArr[i].toCharArray();
+            for (int j = tempArr.length-1 ; j >=0 ; j--) {
+                result.append(tempArr[j]);
+            }
+            if (i != splitArr.length-1) {
+                result.append(" ");
+            }
+        }
+        return result.toString();
     }
 }
 /*

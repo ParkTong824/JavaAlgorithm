1afc73a5306d8c0175a2fd3bba698084f12a4283
Sungwoo824 <scody1219@gmail.com>
2022-05-30 21:52:13 +0900

    add : problem

---
+++ src/leetcode6/S605_CanPlaceFlowers.java
@@ -0,0 +1,11 @@
+package leetcode6;
+
+public class S605_CanPlaceFlowers {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

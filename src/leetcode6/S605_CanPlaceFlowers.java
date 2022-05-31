0bb2b67d1b2a263c9a2711569de15147fa4a0cd9
Sungwoo824 <scody1219@gmail.com>
2022-05-31 21:52:13 +0900

    add : solution

--- src/leetcode6/S605_CanPlaceFlowers.java
+++ src/leetcode6/S605_CanPlaceFlowers.java
@@ -1,11 +1,39 @@
 package leetcode6;
 
 public class S605_CanPlaceFlowers {
-    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        boolean checkStart = false;
+        boolean beforeOne = false;
+        int result = 0;
+        for (int i = 0 ; i < flowerbed.length ; i++) {
+            if (flowerbed[i]==1) {
+                if (checkStart && !beforeOne) {
+                    result++;
+                } else if (!checkStart) {
+                    checkStart = true;
+                }
 
+                beforeOne = true;
+            } else {
+                beforeOne = false;
+            }
+            if (result >= n) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public static void main(String[] args) {
-
+        int[] test = {1,0,0,0,1};
+        System.out.println(canPlaceFlowers(test, 1));
     }
 }
+/*
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+ */

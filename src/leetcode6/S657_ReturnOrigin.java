5a2e42de80bb51d7fb88308b14ecb23613c6b73c
Sungwoo824 <scody1219@gmail.com>
2022-06-05 21:52:13 +0900

    add : problem

---
+++ src/leetcode6/S657_ReturnOrigin.java
@@ -0,0 +1,21 @@
+package leetcode6;
+
+public class S657_ReturnOrigin {
+    public static void main(String[] args) {
+
+    }
+    public boolean judgeCircle(String moves) {
+
+    }
+}
+/*
+Input: moves = "UD"
+Output: true
+Explanation: The robot moves up once, and then down once. All moves have the same magnitude,
+ so it ended up at the origin where it started. Therefore, we return true.
+
+Input: moves = "LL"
+Output: false
+Explanation: The robot moves left twice. It ends up two "moves" to the left of the origin.
+ We return false because it is not at the origin at the end of its moves.
+ */

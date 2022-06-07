2cc820452230ff19da05285be566b952c5fc5f26
Sungwoo824 <scody1219@gmail.com>
2022-06-07 21:52:13 +0900

    add problem

---
+++ src/leetcode6/S682_BaseballGame.java
@@ -0,0 +1,26 @@
+package leetcode6;
+
+public class S682_BaseballGame {
+    public int calPoints(String[] ops) {
+
+    }
+}
+/*
+Input: ops = ["5","2","C","D","+"]
+Output: 30
+Explanation:
+"5" - Add 5 to the record, record is now [5].
+"2" - Add 2 to the record, record is now [5, 2].
+"C" - Invalidate and remove the previous score, record is now [5].
+"D" - Add 2 * 5 = 10 to the record, record is now [5, 10].
+"+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
+The total sum is 5 + 10 + 15 = 30.
+
+Input: ops = ["5","-2","4","C","D","9","+","+"]
+Output: 27
+
+"+" - Record a new score that is the sum of the previous two scores. It is guaranteed there will always be two previous scores.
+"D" - Record a new score that is double the previous score. It is guaranteed there will always be a previous score.
+"C" - Invalidate the previous score, removing it from the record. It is guaranteed there will always be a previous score.
+
+ */

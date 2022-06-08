37c73841b221c17273dc021859428c801cec32a1
Sungwoo824 <scody1219@gmail.com>
2022-06-08 21:52:13 +0900

    add solution

--- src/leetcode6/S682_BaseballGame.java
+++ src/leetcode6/S682_BaseballGame.java
@@ -1,8 +1,33 @@
 package leetcode6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class S682_BaseballGame {
     public int calPoints(String[] ops) {
+        List<Integer> opsList = new ArrayList<>();
+        for (int i = 0 ; i < ops.length ; i++) {
+            switch (ops[i]){
+                case "C":
+                    opsList.remove(opsList.size()-1);
+                    break;
+                case "D":
+                    opsList.add(opsList.get(opsList.size()-1)*2);
+                    break;
+                case "+":
+                    opsList.add(opsList.get(opsList.size()-2)+opsList.get(opsList.size()-1));
+                    break;
+                default:
+                    opsList.add(Integer.parseInt(ops[i]));
+                    break;
+            }
+        }
+        int answer = 0;
+        for (int i = 0 ; i < opsList.size() ; i++) {
+            answer+=opsList.get(i);
+        }
 
+        return answer;
     }
 }
 /*

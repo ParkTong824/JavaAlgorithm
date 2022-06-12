3798b8846e37101182b7a263cea60f3bb3d77d14
Sungwoo824 <scody1219@gmail.com>
2022-06-12 21:52:13 +0900

    add problem

---
+++ src/programmersLevel1/S01_Kakao.java
@@ -0,0 +1,17 @@
+package programmersLevel1;
+
+public class S01_Kakao {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+/*
+id_list	report	k	result
+["muzi", "frodo", "apeach", "neo"]	["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]	2	[2,1,1,0]
+["con", "ryan"]	["ryan con", "ryan con", "ryan con", "ryan con"]	3	[0,0]
+ */

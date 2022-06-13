3a2c59ba95e41f5ba6fe2ac97fc71623f6cdff06
Sungwoo824 <scody1219@gmail.com>
2022-06-13 21:52:13 +0900

    add solution

--- src/leetcode6/S804_MorseCode.java
+++ src/leetcode6/S804_MorseCode.java
@@ -2,7 +2,7 @@ package leetcode6;
 
 public class S804_MorseCode {
     public int uniqueMorseRepresentations(String[] words) {
-
+        return 0;
     }
 
     public static void main(String[] args) {
--- src/programmersLevel1/S01_Kakao.java
+++ src/programmersLevel1/S01_Kakao.java
@@ -1,12 +1,37 @@
 package programmersLevel1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class S01_Kakao {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = {};
+        int[] checkArr = new int[id_list.length];
+        Map<String, Integer> checkMap = new HashMap<>();
+        Map<String, Integer> userMap = new HashMap<>();
+        for (int i = 0 ; i < report.length ; i++) {
+            String[] tempArr = report[i].split(" ");
+            if (checkMap.containsKey(tempArr[1])) {
+                checkMap.put(tempArr[1],checkMap.get(tempArr[1])+1);
+            } else {
+                checkMap.put(tempArr[1],1);
+            }
+
+            if (userMap.containsKey(tempArr[0])) {
+                checkMap.put(tempArr[0],checkMap.get(tempArr[0])+1);
+            } else {
+                checkMap.put(tempArr[0],1);
+            }
+        }
         return answer;
     }
 
     public static void main(String[] args) {
+        Map<String, Integer> test = new HashMap<>();
+        String testString = "test";
+        test.put("test",1);
+
+        System.out.println(test.get("test"));
 
     }
 }

d32687d2915029cd96b47a26f89f94eb98e13f26
Sungwoo <scody1219@gmail.com>
2023-03-26 14:26:58 +0900

    add solution

--- src/programmersLevel0/Solution44.java
+++ src/programmersLevel0/Solution44.java
@@ -1,37 +1,63 @@
 package programmersLevel0;
 
 public class Solution44 {
+    int[][] answerBoard;
     public int solution(int[][] board) {
         int answer = 0;
+        if (board.length == 1 ) {
+            if (board[0][0] == 1) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+        answerBoard = new int[board.length][board[0].length];
         int minusAnswer = 0;
         for (int i = 0 ; i < board.length ; i++) {
             for (int j = 0 ; j < board[0].length ; j++) {
                 if (board[i][j] == 1) {
-                    int isPossible = 0;
+                    int[] isPossibleArr = new int[]{0,0};
                     if (i-1 < 0) {
-                        isPossible++;
+                        isPossibleArr[0] = -1;
                     } else if (i+1 == board.length) {
-                        isPossible++;
+                        isPossibleArr[0] = 1;
                     }
                     if (j-1 < 0) {
-                        isPossible++;
+                        isPossibleArr[1] = -1;
                     } else if (j+1 == board[i].length) {
-                        isPossible++;
+                        isPossibleArr[1] = 1;
                     }
-                    switch (isPossible) {
-                        case 0:
-                            minusAnswer+=8;
-                            break;
-                        case 1:
-                            minusAnswer+=5;
-                            break;
-                        case 2:
-                            minusAnswer+=3;
-                            break;
-                    }
+                    int[] possibleHorizon = checkPossible(isPossibleArr[0], i);
+                    int[] possibleVertical = checkPossible(isPossibleArr[1], j);
+                    indicateDangerArea(possibleHorizon, possibleVertical);
+                }
+            }
+        }
+        for (int i = 0 ; i < board.length ; i++) {
+            for (int j = 0 ; j < board[0].length ; j++) {
+                if (answerBoard[i][j] == 0 ){
+                    answer++;
                 }
             }
         }
         return answer;
+    }
+    public int[] checkPossible(int possibleCheck, int index){
+        switch (possibleCheck) {
+            case -1:
+                return new int[]{index,index+1};
+            case 1:
+                return new int[]{index-1, index};
+            case 0:
+            default:
+                return new int[]{index-1, index, index+1};
+        }
+    }
+    public void indicateDangerArea(int[] possibleHorizon, int[] possibleVertical) {
+        for (int horizon : possibleHorizon) {
+            for (int vertical : possibleVertical) {
+                answerBoard[horizon][vertical] = 1;
+            }
+        }
     }
 }

a8afe7bdd66e1af62a0d8ca0bd1a602d1f52beed
Sungwoo <scody1219@gmail.com>
2023-04-08 14:26:58 +0900

    add solution

--- src/programmersLevel0/Solution57.java
+++ src/programmersLevel0/Solution57.java
@@ -1,2 +1,10 @@
-package programmersLevel0;public class Solution57 {
+package programmersLevel0;
+
+import java.util.Arrays;
+
+public class Solution57 {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        return numbers[numbers.length-1] * numbers[numbers.length-2];
+    }
 }

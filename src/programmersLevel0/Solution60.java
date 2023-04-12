c45cb615cd521f2ec3e05c7b20d932cd56a7d946
Sungwoo <scody1219@gmail.com>
2023-04-12 14:26:58 +0900

    add solution

--- src/programmersLevel0/Solution60.java
+++ src/programmersLevel0/Solution60.java
@@ -2,8 +2,6 @@ package programmersLevel0;
 
 public class Solution60 {
     public int[] solution(int[] numbers, String direction) {
-        int[] answer = {};
-        boolean isLeft = false;
         int tempNumber = 0;
         int pivotNumber = 0;
         if (direction.equals("left")) {
@@ -11,18 +9,24 @@ public class Solution60 {
             for (int i = numbers.length-1 ; i >= 0 ; i--) {
                 if (i-1 >=0) {
                     tempNumber = numbers[i-1];
-                    numbers[i-1] = tempNumber;
+                    numbers[i-1] = pivotNumber;
+                    pivotNumber = tempNumber;
                 } else {
-
+                    numbers[numbers.length-1] = pivotNumber;
                 }
             }
         } else {
+            pivotNumber = numbers[0];
             for (int i = 0 ; i < numbers.length ; i++) {
-
+                if (i+1 <= numbers.length-1) {
+                    tempNumber = numbers[i+1];
+                    numbers[i+1] = pivotNumber;
+                    pivotNumber = tempNumber;
+                } else {
+                    numbers[0] = pivotNumber;
+                }
             }
         }
-
-
-        return answer;
+        return numbers;
     }
 }

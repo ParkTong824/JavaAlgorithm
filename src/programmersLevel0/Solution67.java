69fbd7b63c1ca33c17c744f6ebbf58ff05d379f7
Sungwoo <scody1219@gmail.com>
2023-04-19 14:26:58 +0900

    add solution

---
+++ src/programmersLevel0/Solution67.java
@@ -0,0 +1,13 @@
+package programmersLevel0;
+
+public class Solution67 {
+    public int solution(int hp) {
+        int answer = 0;
+        answer+=hp/5;
+        hp%=5;
+        answer+=hp/3;
+        hp%=3;
+        answer+=hp;
+        return answer;
+    }
+}

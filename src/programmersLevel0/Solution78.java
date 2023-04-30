70e394e41edb8316811889e10bfd4a5483118537
Sungwoo <scody1219@gmail.com>
2023-04-30 14:26:58 +0900

    add solution

---
+++ src/programmersLevel0/Solution78.java
@@ -0,0 +1,19 @@
+package programmersLevel0;
+
+import java.util.Scanner;
+
+public class Solution78 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printStar(n);
+    }
+    public static void printStar(int n) {
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 0; j < i+1 ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

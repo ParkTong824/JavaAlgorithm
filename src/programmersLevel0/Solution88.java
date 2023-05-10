3bf57f7f410758bb15efa9b40b09cde9cbea3ef3
Sungwoo <scody1219@gmail.com>
2023-05-10 14:26:58 +0900

    add solution

--- src/programmersLevel0/Solution88.java
+++ src/programmersLevel0/Solution88.java
@@ -1,2 +1,25 @@
-package programmersLevel0;public class Solution88 {
+package programmersLevel0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution88 {
+    public static int[] solution(int n) {
+        int answerLength = n%2 == 1 ? (n/2+1):(n/2);
+        int[] answer = new int[answerLength];
+        int index = 0;
+        for (int i = 1 ; i <= n ; i++) {
+            if (i%2 == 1) {
+                answer[index] = i;
+                index++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 15;
+        solution(n);
+    }
 }

d4091f88b85a637a6c656a88c5bb8081c492f1e5
Sungwoo <scody1219@gmail.com>
2023-05-11 22:30:43 +0900

    add problem

---
+++ src/programmersLevel0/Solution89.java
@@ -0,0 +1,14 @@
+package programmersLevel0;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Solution89 {
+    public int solution(int[] array) {
+        int answer = 0;
+        Map<Integer,Integer> keyMap = Arrays.stream(array).collect()
+        return answer;
+    }
+}

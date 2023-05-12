5ebd1225ef8647a6d5391e9f05fb62afd93b7dc4
Sungwoo <scody1219@gmail.com>
2023-05-12 14:26:58 +0900

    add solution

--- src/programmersLevel0/Solution89.java
+++ src/programmersLevel0/Solution89.java
@@ -1,14 +1,28 @@
 package programmersLevel0;
 
-import java.util.Arrays;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Solution89 {
     public int solution(int[] array) {
         int answer = 0;
-        Map<Integer,Integer> keyMap = Arrays.stream(array).collect()
-        return answer;
+        Map<Integer,Integer> keyMap = new HashMap<>();
+        for (int number : array) {
+            if (keyMap.containsKey(number)) {
+                keyMap.put(number, keyMap.get(number)+1);
+            } else {
+                keyMap.put(number, 1);
+            }
+        }
+        List<Integer> valueList = keyMap.values().stream().sorted().collect(Collectors.toList());
+        try {
+            if ( valueList.get(valueList.size()-1) == valueList.get(valueList.size()-2) ) {
+                return -1;
+            } else {
+                return valueList.get(valueList.size()-1);
+            }
+        } catch (IndexOutOfBoundsException e) {
+            return valueList.get(valueList.size()-1);
+        }
     }
 }

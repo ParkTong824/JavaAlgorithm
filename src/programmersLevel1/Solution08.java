80b1a9f1a4550f4108cf10f07567be7371acb55f
Sungwoo <scody1219@gmail.com>
2023-08-06 20:16:32 +0900

    add solution

---
+++ src/programmersLevel1/Solution08.java
@@ -0,0 +1,24 @@
+package programmersLevel1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution08 {
+    public int solution(int[] nums) {
+        int distinctSize = Arrays.stream(nums).distinct().boxed().collect(Collectors.toList()).size();
+        return Math.min(nums.length / 2, distinctSize);
+    }
+}
+/*
+제한사항
+nums는 폰켓몬의 종류 번호가 담긴 1차원 배열입니다.
+nums의 길이(N)는 1 이상 10,000 이하의 자연수이며, 항상 짝수로 주어집니다.
+폰켓몬의 종류 번호는 1 이상 200,000 이하의 자연수로 나타냅니다.
+가장 많은 종류의 폰켓몬을 선택하는 방법이 여러 가지인 경우에도, 선택할 수 있는 폰켓몬 종류 개수의 최댓값 하나만 return 하면 됩니다.
+입출력 예
+nums	result
+[3,1,2,3]	2
+[3,3,3,2,2,4]	3
+[3,3,3,2,2,2]	2
+ */

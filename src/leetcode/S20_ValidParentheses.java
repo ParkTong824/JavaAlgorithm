62ae959aa267b9de7708274ea1608fd2d319a2c3
Sungwoo <scody1219@gmail.com>
2024-08-01 13:53:41 +0900

    add solution

--- src/leetcode/S20_ValidParentheses.java
+++ src/leetcode/S20_ValidParentheses.java
@@ -1,47 +1,71 @@
 package leetcode;
 
+import java.util.Stack;
+
 public class S20_ValidParentheses {
     public boolean isValid(String s) {
-        String[] sSplit = s.split("");
-        String[] before = new String[s.length()];
-        int beforeIndex = 0;
-        boolean isValid = false;
-        if (s.length()==1){
-            return false;
-        }
-        for (int i = 0; i < sSplit.length ; i++){
-            if (sSplit[i].equals("[")){
-                before[beforeIndex]="[";
-                beforeIndex++;
-            }else if (sSplit[i].equals("{")){
-                before[beforeIndex]="{";
-                beforeIndex++;
-            }else if (sSplit[i].equals("(")){
-                before[beforeIndex]="(";
-                beforeIndex++;
-            }else {
-                if (beforeIndex==0){
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
                     return false;
                 }
-                beforeIndex--;
-                if (sSplit[i].equals("]")){
-                    if (!before[beforeIndex].equals("[")){
-                        return false;
-                    }
-                }else if (sSplit[i].equals("}")){
-                    if (!before[beforeIndex].equals("{")){
-                        return false;
-                    }
-                }else if (sSplit[i].equals(")")){
-                    if (!before[beforeIndex].equals("(")){
-                        return false;
-                    }
+                char pop = stack.pop();
+                switch (c) {
+                    case ')':
+                        if (pop != '(') {
+                            return false;
+                        }
+                        break;
+                    case '}':
+                        if (pop != '{') {
+                            return false;
+                        }
+                        break;
+                    case ']':
+                        if (pop != '[') {
+                            return false;
+                        }
+                        break;
                 }
-                isValid = true;
             }
-
         }
-        return isValid;
+        if (stack.isEmpty()) {
+            return true;
+        }
+        return false;
     }
 
 }
+
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+
+Input: s = "()"
+Output: true
+
+Input: s = "()[]{}"
+Output: true
+
+Input: s = "(]"
+Output: false
+
+Input: s = "([])"
+Output: true
+
+
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+ */

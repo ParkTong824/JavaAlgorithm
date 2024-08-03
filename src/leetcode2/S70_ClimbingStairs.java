635a7632e39a83a59103a6a966883873d2df04a7
Sungwoo <scody1219@gmail.com>
2024-08-03 13:53:41 +0900

    add solution

--- src/leetcode2/S70_ClimbingStairs.java
+++ src/leetcode2/S70_ClimbingStairs.java
@@ -2,28 +2,27 @@ package leetcode2;
 
 public class S70_ClimbingStairs {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 6;
         System.out.println(climbStairs(n));
     }
     public static int climbStairs(int n) {
-        return climbStair(0,n);
-    }
-    public static int climbStair(int i, int n){
-        if (i > n){
-            return 0;
-        }
-        if (i == n){
-            return 1;
-        }
-        return climbStair(i+1,n)+climbStair(i+2,n);
+        double sqrt = Math.sqrt(5);
+        return (int) ( ( Math.pow( (1+sqrt)/ 2, n+1) - Math.pow((1-sqrt)/ 2, n+1) ) / sqrt );
     }
 }
 /*
+3
+1 2 3
+0 1 2 3  4   5    6     7      8
+1 1 2 3  5   8   13    17     23
+x 0 1  3   6   10    15     21
+  0 1 12 123 1234 12345 123456
 Input: n = 2
 Output: 2
 Explanation: There are two ways to climb to the top.
 1. 1 step + 1 step
 2. 2 steps
+10
 
 Input: n = 3
 Output: 3
@@ -31,6 +30,7 @@ Explanation: There are three ways to climb to the top.
 1. 1 step + 1 step + 1 step
 2. 1 step + 2 steps
 3. 2 steps + 1 step
+11
 
 4  =>  5
 1 1 1 1
@@ -38,6 +38,7 @@ Explanation: There are three ways to climb to the top.
 1 2 1
 1 1 2
 2 2
+100
 
 5  => 8
 1 1 1 1 1
@@ -49,5 +50,18 @@ Explanation: There are three ways to climb to the top.
 2 1 2
 1 2 2
 
+6 => 12
+1 1 1 1 1 1
+1 1 1 1 2
+1 1 1 2 1
+1 1 2 1 1
+1 2 1 1 1
+2 1 1 1 1
+1 1 2 2
+1 2 1 2
+2 1 1 2
+1 2 2 1
+2 1 2 1
+2 2 1 1
 1 <= n <= 45
  */

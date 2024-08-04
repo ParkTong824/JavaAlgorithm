936296d8cb3cc905687c93dba8670e16fba06345
Sungwoo <scody1219@gmail.com>
2024-08-04 13:53:41 +0900

    add solution

--- src/leetcode2/S88_MergeSortArr.java
+++ src/leetcode2/S88_MergeSortArr.java
@@ -14,46 +14,40 @@ public class S88_MergeSortArr {
         System.out.println(Arrays.toString(nums1));
     }
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (nums1.length!=m) {
-            if (nums1.length==n){
-                nums1 = Arrays.copyOfRange(nums2,0,n);
+        int[] answer = new int[m + n];
+        int num1Index = 0;
+        int num2Index = 0;
+        while (num1Index < m || num2Index < n) {
+            if (num1Index == m) {
+                answer[num1Index+num2Index] = nums2[num2Index];
+                num2Index++;
+                continue;
+            } else if (num2Index == n) {
+                answer[num1Index+num2Index] = nums1[num1Index];
+                num1Index++;
+                continue;
+            }
+
+            if (nums1[num1Index] > nums2[num2Index]) {
+                answer[num1Index+num2Index] = nums2[num2Index];
+                num2Index++;
             } else {
-                int[] tempArr = nums1.clone();
-                int firstIndex = 0;
-                int secondIndex = 0;
-                int firstNum;
-                int secondNum;
-                while (m>0||n>0){
-                    if (m>0){
-                        firstNum = tempArr[firstIndex];
-                        if (n>0){
-                            secondNum=nums2[secondIndex];
-                            if (firstNum>secondNum){
-                                nums1[firstIndex+secondIndex]=secondNum;
-                                secondIndex++;
-                                n--;
-                            } else {
-                                nums1[firstIndex+secondIndex]=firstNum;
-                                firstIndex++;
-                                m--;
-                            }
-                        } else {
-                            nums1[firstIndex+secondIndex]=firstNum;
-                            firstIndex++;
-                        }
-                    } else {
-                        secondNum=nums2[secondIndex];
-                        nums1[firstIndex+secondIndex]=secondNum;
-                        secondIndex++;
-                        n--;
-                    }
-                }
+                answer[num1Index+num2Index] = nums1[num1Index];
+                num1Index++;
             }
         }
-
+        System.arraycopy(answer, 0, nums1, 0, answer.length);
     }
 }
 /*
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
+ and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
 Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 Output: [1,2,2,3,5,6]
 Explanation: The arrays we are merging are [1,2,3] and [2,5,6].

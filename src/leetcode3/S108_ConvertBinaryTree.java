bdc828a1421026efda9e45481bd276366d7229a6
Sungwoo <scody1219@gmail.com>
2024-08-09 13:53:41 +0900

    add solution

--- src/leetcode3/S108_ConvertBinaryTree.java
+++ src/leetcode3/S108_ConvertBinaryTree.java
@@ -1,31 +1,59 @@
 package leetcode3;
 
 public class S108_ConvertBinaryTree {
-    public TreeNode convert(int[] nums,int left,int right){
-        // important base case
-        if(left>right) return null;
-        //finding mid value
-        int mid=(left+right)/2;
-        //creating Node for mid element
-        TreeNode temp=new TreeNode(nums[mid]);
-        //recursively calling for its left and right elements
-        temp.left=convert(nums,left,mid-1);
-        temp.right=convert(nums,mid+1,right);
-        return temp;
+    public static void main(String[] args) {
+        int[] nums = {-10, -3,0,5,9};
+
+        sortedArrayToBST(nums);
     }
-    public TreeNode sortedArrayToBST(int[] nums) {
-        return convert(nums,0,nums.length-1);
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        TreeNode root = new TreeNode(nums[nums.length/2]);
+
+        if (nums.length > 1) {
+            addChildNode(nums, root, nums.length/2, 0, nums.length-1);
+        }
+
+        return root;
+    }
+
+    public static void addChildNode(int[] nums, TreeNode parentsNode, int parentsIndex, int startIndex, int lastIndex) {
+        if (startIndex < parentsIndex) {
+            int firstIndex = (parentsIndex-1 - startIndex)/2 + startIndex;
+            TreeNode firstNode = new TreeNode(nums[firstIndex]);
+            if (firstNode.val < parentsNode.val) {
+                parentsNode.left = firstNode;
+            } else {
+                parentsNode.right = firstNode;
+            }
+            if (firstIndex < parentsIndex-1) {
+                addChildNode(nums, firstNode, firstIndex, startIndex, parentsIndex-1);
+            }
+        }
+        if (parentsIndex < lastIndex) {
+            int secondIndex = lastIndex - (lastIndex - (parentsIndex+1))/2;
+            TreeNode secondNode = new TreeNode(nums[secondIndex]);
+            if (secondNode.val < parentsNode.val) {
+                parentsNode.left = secondNode;
+            } else {
+                parentsNode.right = secondNode;
+            }
+            if (secondIndex <= lastIndex) {
+                addChildNode(nums, secondNode, secondIndex,parentsIndex+1, lastIndex);
+            }
+        }
     }
 }
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
+/*
+[0,-10,9,null,null,5]
+Given an integer array nums where the elements are sorted in ascending order, convert it to a
+height-balanced
+ binary search tree.
+
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+ */

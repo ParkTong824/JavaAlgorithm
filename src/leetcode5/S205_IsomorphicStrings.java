305a566ba50159acee8268f262074a0d2a931212
Sungwoo <scody1219@gmail.com>
2024-08-29 13:53:41 +0900

    add solution

--- src/leetcode5/S205_IsomorphicStrings.java
+++ src/leetcode5/S205_IsomorphicStrings.java
@@ -1,14 +1,37 @@
 package leetcode5;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class S205_IsomorphicStrings {
-    public boolean isIsomorphic(String s, String t) {
-        Set<Character> checkSet = new HashSet<>();
+    public static void main(String[] args) {
+        String s = "badc";
+        String t = "baba";
+
+        System.out.println(isIsomorphic(s,t));
+    }
+    public static boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> keyMap = new HashMap<>();
+        Map<Character, Character> valueMap = new HashMap<>();
         char[] chars = s.toCharArray();
         for (int i = 0 ; i < chars.length ; i++) {
-            
+            char sChar = chars[i];
+            char tChar = t.charAt(i);
+
+            if (keyMap.containsKey(sChar)) {
+                if (keyMap.get(sChar) != tChar ) {
+                    return false;
+                }
+            } else if (valueMap.containsKey(tChar)) {
+                if (valueMap.get(tChar) != sChar ) {
+                    return false;
+                }
+            } else {
+                keyMap.put(sChar, tChar);
+                valueMap.put(tChar, sChar);
+            }
         }
 
         return true;
@@ -16,6 +39,9 @@ public class S205_IsomorphicStrings {
 }
 
 /*
+"badc"
+t =
+"baba"
 Input: s = "egg", t = "add"
 Output: true
 Example 2:

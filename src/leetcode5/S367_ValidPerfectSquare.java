7bf1cf1baa4a90c619715e44d8f76cb0a7968164
Sungwoo <scody1219@gmail.com>
2024-09-24 13:53:41 +0900

    add solution

--- src/leetcode5/S367_ValidPerfectSquare.java
+++ src/leetcode5/S367_ValidPerfectSquare.java
@@ -1,32 +1,16 @@
 package leetcode5;
 
 public class S367_ValidPerfectSquare {
-    public static boolean isPerfectSquare(int num) {
-        int numLength = (int)(Math.log10(num)+1);
-        int startNum = 1;
-        int answerLength = 0;
-        if (numLength>2){
-            if (numLength%2==1){
-                answerLength=numLength/2+1;
-            } else {
-                answerLength=numLength/2;
-            }
-            for (int i = 1 ; i < answerLength ; i++){
-                startNum*=10;
-            }
-        }
-        while (true){
-            int compareNum = startNum*startNum;
-            if (num==compareNum){
+    public boolean isPerfectSquare(int num) {
+        int pivotNum = 1;
+        while (Math.pow(pivotNum,2) <= num) {
+            if (Math.pow(pivotNum,2) == num) {
                 return true;
-            } else if (num<compareNum) {
-                return false;
-            } else if (num>compareNum*compareNum) {
-                startNum = compareNum+1;
             } else {
-                    startNum++;
+                pivotNum++;
             }
         }
+        return false;
     }
 
     public static void main(String[] args) {
@@ -35,9 +19,23 @@ public class S367_ValidPerfectSquare {
     }
 }
 /*
+Given a positive integer num, return true if num is a perfect square or false otherwise.
+
+A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+
+You must not use any built-in library function, such as sqrt.
+
+
+
+Example 1:
+
 Input: num = 16
 Output: true
+Explanation: We return true because 4 * 4 = 16 and 4 is an integer.
+Example 2:
 
 Input: num = 14
 Output: false
+Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an integer.
+
  */

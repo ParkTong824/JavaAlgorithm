4feadb0419148c82ba7bb30d0d2e8e4775788ba8
Sungwoo <scody1219@gmail.com>
2024-09-26 13:53:41 +0900

    add solution

--- src/leetcode5/S401_BinaryWatch.java
+++ src/leetcode5/S401_BinaryWatch.java
@@ -5,11 +5,42 @@ import java.util.List;
 
 public class S401_BinaryWatch {
     public List<String> readBinaryWatch(int turnedOn) {
-        if (turnedOn==0 || turnedOn>=9) {
-            return new ArrayList<>();
+        List<String> time = new ArrayList<>();
+
+        for (int h=0; h<12; h++){
+            for (int m=0; m<60; m++){
+                if (Integer.bitCount(h * 64 + m) == turnedOn)    time.add(String.format("%d:%02d", h, m));
+            }
         }
-        int hourMax = 3;
-        int minMax = 5;
-        return null;
+
+        return time;
     }
 }
+/*
+A binary watch has 4 LEDs on the top to represent the hours (0-11),
+and 6 LEDs on the bottom to represent the minutes (0-59).
+Each LED represents a zero or one, with the least significant bit on the right.
+
+For example, the below binary watch reads "4:51".
+
+
+Given an integer turnedOn which represents the number of LEDs that are currently on (ignoring the PM),
+return all possible times the watch could represent. You may return the answer in any order.
+
+The hour must not contain a leading zero.
+
+For example, "01:00" is not valid. It should be "1:00".
+The minute must consist of two digits and may contain a leading zero.
+
+For example, "10:2" is not valid. It should be "10:02".
+
+
+Example 1:
+
+Input: turnedOn = 1
+Output: ["0:01","0:02","0:04","0:08","0:16","0:32","1:00","2:00","4:00","8:00"]
+Example 2:
+
+Input: turnedOn = 9
+Output: []
+ */

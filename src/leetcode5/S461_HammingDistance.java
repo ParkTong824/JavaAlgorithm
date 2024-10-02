329900a0a05f2d0f4928525c80e78d9f87568b4a
Sungwoo <scody1219@gmail.com>
2024-10-02 13:53:41 +0900

    add solution

--- src/leetcode5/S461_HammingDistance.java
+++ src/leetcode5/S461_HammingDistance.java
@@ -2,35 +2,39 @@ package leetcode5;
 
 public class S461_HammingDistance {
     public int hammingDistance(int x, int y) {
-        int xNum = 0 ;
-        int yNum = 0 ;
-        boolean xCheck = false;
-        boolean yCheck = false;
-        int index = 0 ;
-        while (!xCheck && !yCheck) {
-            if (!xCheck&&Math.pow(2,index)>=x){
-                if (Math.pow(2,index)==x) {
-                    xNum=index;
-                    xCheck = true;
-                } else {
-                    xNum=index-1;
-                    xCheck = true;
-                }
-            }
-            if (!yCheck&&Math.pow(2,index)>=y) {
-                if (Math.pow(2,index)==y) {
-                    yNum=index;
-                    yCheck = true;
-                } else {
-                    yNum = index-1;
-                    yCheck = true;
-                }
-            }
+        int xor = x ^ y;
+        int answer = 0;
+
+        for (int i = 0; i < 32; i++) {
+            answer += (xor >> i) & 1;
         }
-        return Math.abs(yNum-xNum);
+
+        return answer;
     }
 
     public static void main(String[] args) {
 
     }
 }
+/*
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, return the Hamming distance between them.
+
+
+
+Example 1:
+
+Input: x = 1, y = 4
+Output: 2
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+The above arrows point to positions where the corresponding bits are different.
+Example 2:
+
+Input: x = 3, y = 1
+Output: 1
+
+ */
